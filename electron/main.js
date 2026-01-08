4ac62e661c6411a48cb52bd5484282d560c80bb3
jschomchoey <tpcc.general@gmail.com>
2026-01-09 01:06:11 +0700

    feat: get usb list

---
+++ electron/main.js
@@ -0,0 +1,52 @@
+import { app, BrowserWindow, ipcMain } from "electron";
+import { fileURLToPath } from "url";
+import path from "path";
+import drivelist from "drivelist";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadURL("http://localhost:5173");
+  win.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+  ipcMain.handle("get-disks", async () => {
+    try {
+      const drives = await drivelist.list();
+      const filteredDrives = drives.filter(
+        (drive) => !drive.isSystem && drive.isRemovable && drive.isUSB
+      );
+      //   console.log(filteredDrives);
+      return filteredDrives; // Return filtered disks
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  });
+
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
